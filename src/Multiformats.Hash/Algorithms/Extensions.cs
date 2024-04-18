be9f6e4601bdfd145f1bbaa745dcb7aa892d6c1b
Alexey Osipov <me@flcl.me>
2024-04-18 10:11:06 +0300

    Clean up

--- src/Multiformats.Hash/Algorithms/Extensions.cs
+++ src/Multiformats.Hash/Algorithms/Extensions.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Org.BouncyCastle.Crypto;
 
 namespace Multiformats.Hash.Algorithms
--- src/Multiformats.Hash/Algorithms/MURMUR3.cs
+++ src/Multiformats.Hash/Algorithms/MURMUR3.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Composition;
-using System.Linq;
 using System.Security.Cryptography;
 using Murmur;
 
